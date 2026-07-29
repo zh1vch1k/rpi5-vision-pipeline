985866f065ded01b9da738743bf9810dff602142
zh1vch1k <kyseliov.illia@gmail.com>
2026-07-29 16:08:44 +0300

    feat: Add C++ buffer reader

---
+++ shm_memory_reader.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fcntl.h>        
+#include <sys/mman.h>    
+#include <semaphore.h>   
+#include <system_error>
+#include <unistd.h> 
+#include <cstdint>    
+
+#include <opencv2/opencv.hpp>
+
+#include "frame_metadata.hpp"
+
+class ShmReader { 
+private: 
+
+    static constexpr const char* BUFFER_PATH = "rpi5_pipeline";
+    static constexpr const char* SEM_NAME = "/rpi5_semaphore";
+
+    static constexpr size_t TOTAL_BYTES = sizeof(FrameMetadata) + (640*640*3);
+
+    void* mmap_ptr = nullptr;
+    sem_t* sem = nullptr;
+
+public:
+
+int init() {
+    int fd = shm_open(BUFFER_PATH, O_RDONLY, 0666);
+        if (fd == -1) {
+            std::cerr << "[C++] Error: shm_open hasn't found the path'" << BUFFER_PATH << "'!" << std::endl;
+            return -1;
+        }
+
+        this->mmap_ptr = mmap(nullptr, TOTAL_BYTES, PROT_READ, MAP_SHARED, fd, 0);
+        close(fd);
+
+        if (this->mmap_ptr == MAP_FAILED) {
+            std::cerr << "[C++] Error: mmap crashed!" << std::endl;
+            this->mmap_ptr = nullptr;
+            return -1;
+        }
+
+        this->sem = sem_open(SEM_NAME, 0);
+        if (this->sem == SEM_FAILED) {
+            std::cerr << "[C++] Error: sem_open semophore hasn't been found'" << SEM_NAME << "'!" << std::endl;
+            munmap(this->mmap_ptr, TOTAL_BYTES);
+            this->mmap_ptr = nullptr;
+            return -1;
+        }
+
+        return 0;
+}
+
+cv::Mat readFrame() {
+    sem_wait(sem);
+
+    auto* meta = static_cast<FrameMetadata*>(mmap_ptr);
+    uint8_t* pixels = static_cast<uint8_t*>(mmap_ptr) + sizeof(FrameMetadata);
+
+    return cv::Mat(meta->height, meta->width, CV_8UC3, pixels);
+}
+
+~ShmReader() {
+    if (mmap_ptr && mmap_ptr != MAP_FAILED) {
+        munmap(this->mmap_ptr, TOTAL_BYTES);
+        this->mmap_ptr = nullptr;
+    }
+
+    if (sem && sem != SEM_FAILED) {
+        sem_close(this->sem);
+    }
+}
+
+};
--- shm_memory_reader.py
+++ shm_memory_reader.py
@@ -1,10 +1,10 @@
 import struct
+import numpy as np
 import posix_ipc
 from multiprocessing.shared_memory import SharedMemory
 
-
-
 BUFFER_PATH: str = 'rpi5_pipeline'
+SEM_NAME : str = '/rpi5_semaphore'
 
 #3 channels for image 640*640 in bytes + 32 bits of metadata
 FRAME_SIZE = 640 * 640 * 3  
@@ -12,16 +12,24 @@ HEADER_SIZE = 32
 TOTAL_BYTES = HEADER_SIZE + FRAME_SIZE
 
 shm = SharedMemory(name=BUFFER_PATH, create=True, size=TOTAL_BYTES)
-
-def write_frame(BUFFER_PATH, frame_id, timestamp_ns, frame):
-    height = frame.shape[0]
-    width = frame.shape[1]
-    channels = frame.shape[2]
-    reserved = 0
-
-    frame_metadata = struct.pack('=QIIIQI', frame_id, width, height, channels, timestamp_ns, reserved)
-
-    shm.buf[0: 32] = frame_metadata
-    shm.buf[32: TOTAL_BYTES] = memoryview(frame)
+sem = posix_ipc.Semaphore(
+    SEM_NAME, flags=posix_ipc.O_CREAT, initial_value=0
+)
 
 
+def write_frame(frame_id: np.ndarray, timestamp_ns: int, frame: int):
+    height, width, channels = frame.shape
+
+    frame_metadata = struct.pack('=QIIIQI', frame_id, width, height, channels, timestamp_ns, 0)
+
+    shm.buf[0: HEADER_SIZE] = frame_metadata
+    shm.buf[HEADER_SIZE: HEADER_SIZE + frame.nbytes] = memoryview(frame)
+
+    sem.release()
+
+
+def cleanup():
+    shm.close()
+    shm.unlink()
+    sem.close()
+    sem.unlink()
