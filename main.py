4229b1d6ea5ada41b265eac25850378c205b0f86
zh1vch1k <kyseliov.illia@gmail.com>
2026-09-14 22:52:57 +0300

    refactor: renamed lucas_canade.py -> detection_recognition_pipeline.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
-# import lucas_canade
-import testament as te
+import detection_recognition_pipeline as dpr
 import cv2 as cv
 import shm_memory_writer as mem_writer
 import queue
@@ -12,13 +11,13 @@ if __name__ == '__main__':
     in_queue = queue.Queue(maxsize=1)
     out_queue = queue.Queue(maxsize=1)
 
-    inference_thread = threading.Thread(target = te.inference, 
+    inference_thread = threading.Thread(target = dpr.inference, 
                                         args=(in_queue, out_queue), 
                                         daemon=True)
     inference_thread.start()
 
     try:
-        for frame in te.frame_process(in_queue, out_queue):
+        for frame in dpr.frame_process(in_queue, out_queue):
             mem_writer.write_frame(frame_id, time.time_ns(), frame)
             frame_id += 1
             
